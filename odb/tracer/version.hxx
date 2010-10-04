c96eb97b4e13771697b466f2bbe74621635bbe7d
Boris Kolpackov <boris@codesynthesis.com>
2010-10-04 14:40:48 +0200

    Bump version to 1.1.0.a1

--- odb/tracer/version.hxx
+++ odb/tracer/version.hxx
@@ -29,15 +29,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 10000
+#if ODB_VERSION != 10001
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-tracer version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_TRACER_VERSION     1000000
-#define LIBODB_TRACER_VERSION_STR "1.0.0"
+#define LIBODB_TRACER_VERSION     1009901
+#define LIBODB_TRACER_VERSION_STR "1.1.0.a1"
 
 #include <odb/post.hxx>
 
