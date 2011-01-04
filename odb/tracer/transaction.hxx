992ad51cc50f4703945338ae7821e47bda35c6f1
Boris Kolpackov <boris@codesynthesis.com>
2011-01-04 16:57:27 +0200

    Copyright update

--- odb/tracer/transaction.hxx
+++ odb/tracer/transaction.hxx
@@ -1,6 +1,6 @@
 // file      : odb/tracer/transaction.hxx
 // author    : Boris Kolpackov <boris@codesynthesis.com>
-// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 #ifndef ODB_TRACER_TRANSACTION_HXX
